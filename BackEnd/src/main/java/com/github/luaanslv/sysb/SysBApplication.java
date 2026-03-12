ed5880af87dabca76253d42de1a1493177e40aae
luaanslv <luaan.silva117@gmail.com>
2026-03-11 21:06:48 -0300

    refact: organizando pastas

---
+++ BackEnd/src/main/java/com/github/luaanslv/sysb/SysBApplication.java
@@ -0,0 +1,13 @@
+package com.github.luaanslv.sysb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SysBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SysBApplication.class, args);
+       
+    }
+
+}
